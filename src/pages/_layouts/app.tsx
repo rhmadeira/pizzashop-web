fb87784f79f2b1fc4c2955a0055de599d3d02291
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-10-26 00:16:18 -0300

    refactor: Remove unused ESLint configuration and Prettier plugin
    Remove unused ESLint configuration and Prettier plugin from the project.
    
    feat: Add Vite environment declaration file
    
    Add Vite environment declaration file to the project.
    
    refactor: Update app layout and auth layout
    
    Update the app layout and auth layout components to improve the structure and styling.
    
    chore: Update dependencies
    
    Update the dependencies in the project, including adding the "@radix-ui/react-separator" package.
    
    feat: Add Header component
    
    Add the Header component to the project, which includes a navigation menu and a separator.
    
    feat: Add NavLink component
    
    Add the NavLink component to the project, which provides a styled link for navigation.
    
    feat: Add Separator component
    
    Add the Separator component to the project, which is a UI component for visual separation.

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,15 +1,11 @@
+import { Header } from "@/components/header";
 import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
   return (
-    <div>
-      <h1 className="text-red-600">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-        laudantium nam alias ipsum veritatis excepturi vitae distinctio
-        perspiciatis. Sint libero debitis ipsam magnam, earum quasi corporis
-        accusantium nobis et consequatur.
-      </h1>
-      <div>
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
