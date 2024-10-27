4f081932f9f616db7d2a4e9044bbf25876b23282
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-10-27 15:52:00 -0300

    refactor: Remove unused ESLint configuration and Prettier plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import { router } from "./pages/routes";
 
 export function App() {
   return (
--- src/pages/routes/index.tsx
+++ src/pages/routes/index.tsx
@@ -1,10 +1,12 @@
-import { Dashboard } from "@/pages/app/dashboard";
-import { SignIn } from "@/pages/auth/sign-in";
-import { Documents } from "@/pages/app/documents";
 import { createBrowserRouter } from "react-router-dom";
-import { AppLayout } from "./pages/_layouts/app";
-import { AuthLayout } from "./pages/_layouts/auth";
-import { SignUp } from "./pages/auth/sign-up";
+
+import { Dashboard } from "@/pages/app/dashboard";
+import { Documents } from "@/pages/app/helpers/documents";
+import { SignIn } from "@/pages/auth/sign-in";
+import { SignUp } from "@/pages/auth/sign-up";
+import { AppLayout } from "@/pages/_layouts/app";
+import { AuthLayout } from "@/pages/_layouts/auth";
+import { Orders } from "@/pages/app/orders/orders";
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Dashboard />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
       },
     ],
   },
